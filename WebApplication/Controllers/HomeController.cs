9c705269a5af3b74f54b2fe101e05c6d34058482
shalina2 <shalom.belaineh@gmail.com>
2019-01-23 11:43:54 -0800

    Add project files.

---
+++ WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Controllers
+{
+    
+    public class HomeController:Controller
+    {
+        [HttpGet]
+
+        public IActionResult index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(int firstNumber, int secondNumber)
+        {
+            return RedirectToAction("Results", new {firstNumber,secondNumber });
+        }
+        [HttpGet]
+        public IActionResult Results(int first)
+    }
+}
---
+++ shalina2/lab07collection/UnitTest1.cs
@@ -0,0 +1,50 @@
+using lab07_collection;
+using System;
+using Xunit;
+
+namespace lab07collection
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void addcard()
+        {
+            Deck<Card> Deck = new Deck<Card>();
+            Card card1 = new Card("9", Card.Type.Spades );
+            Deck.Add(card1);
+            int a = Deck.Count();
+            Assert.Equal(1, a);
+        }
+        [Fact]
+        public void DontRemovecardwhenexists()
+        {
+            Deck<Card> Deck = new Deck<Card>();
+            Card card1 = new Card("2", Card.Type.Diamonds);
+            Card card2 = new Card("5", Card.Type.Hearts);
+            Deck.Add(card1);
+            Deck.Add(card2);
+            Deck.Remove(card2);
+            Assert.Null(Deck.Remove(card2));
+            
+        }
+        [Fact]
+        public void RemovewhenExist()
+        {
+            Deck<Card> Deck = new Deck<Card>();
+            Card card1 = new Card("2", Card.Type.Diamonds);
+            Card card2 = new Card("5", Card.Type.Hearts);
+            Deck.Add(card1);
+            Deck.Add(card2);
+            Card[] expected = new Card[] { card1 };
+            Assert.Equal(expected[0].CardNum, Deck.Remove(card2)[0].CardNum );
+        }
+        [Fact]
+        public void getinfo()
+        {
+            Deck<Card> Deck = new Deck<Card>();
+            Card card1 = new Card("2", Card.Type.Diamonds);
+            Deck.Add(card1);
+            Assert.Equal("2",card1.CardNum );
+        }
+    }
+}
